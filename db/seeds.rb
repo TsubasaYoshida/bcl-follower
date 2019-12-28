0e200d159279af0a1be17a65527f21f56966f949
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-12-28 22:06:37 +0900

    Teamテーブルへのseed.rbを作成する

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+[
+    ['富山GRNサンダーバーズ', 'T_birds', '富山'],
+    ['新潟アルビレックスBC', 'Albi_BC_PR', '新潟'],
+    ['信濃グランセローズ', 'GrazieClub', '信濃'],
+    ['石川ミリオンスターズ', 'I_Million_Stars', '石川'],
+    ['オセアン滋賀ユナイテッドBC', 'shigaunitedbc', '滋賀'],
+    ['茨城アストロプラネッツ', 'ibaraki_planets', '茨城'],
+    ['福島レッドホープス', 'F_REDHOPES', '福島'],
+    ['埼玉武蔵ヒートベアーズ', 'musashi_bears', '埼玉'],
+    ['栃木ゴールデンブレーブス', 'tochigi_gb', '栃木'],
+    ['群馬ダイヤモンドペガサス', 'kyumaraima', '群馬'],
+    ['神奈川フューチャードリームス', 'k_futuredreams', '神奈川'],
+    ['福井ワイルドラプターズ', 'Fukui_Raptors', '福井']
+].each do |name, screen_name, short_name|
+  Team.create!(
+      {name: name, screen_name: screen_name, short_name: short_name}
+  )
+end
