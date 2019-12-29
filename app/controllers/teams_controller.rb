92c6e92972a6d395456e016ad456baaed2a9a291
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-12-30 00:27:09 +0900

    チーム一覧ページ(トップページ)を実装する

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,13 @@
 class TeamsController < ApplicationController
 
   def index
-    @teams = Team.all
+    @followers_today = Follower.where(created_at: Time.now.all_day).order(followers_count: :desc)
+    @followers_yesterday = Follower.where(created_at: 1.day.ago.all_day)
+
+    if @followers_today.size < 1
+      @followers_today = Follower.where(created_at: 1.day.ago.all_day).order(followers_count: :desc)
+      @followers_yesterday = Follower.where(created_at: 2.day.ago.all_day)
+    end
   end
 
   def show
