7d779a74602e87796cb4162a4459d0a67cfce81a
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-12-30 00:36:23 +0900

    不要になったルーティングを削除する

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,11 +1,2 @@
 class FollowersController < ApplicationController
-  def index
-    @followers_today = Follower.where(created_at: Time.now.all_day).order(followers_count: :desc)
-    @followers_yesterday = Follower.where(created_at: 1.day.ago.all_day)
-
-    if @followers_today.size < 1
-      @followers_today = Follower.where(created_at: 1.day.ago.all_day).order(followers_count: :desc)
-      @followers_yesterday = Follower.where(created_at: 2.day.ago.all_day)
-    end
-  end
 end
